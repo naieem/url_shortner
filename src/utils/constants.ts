8d0a61d20bb4911270eb8fe260be23e3b12e447d
Naieem Mahmud Supto <naieemsupto@gmail.com>
2022-11-14 20:53:05 +0600

    [Terms] Super admin terms added to the constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,6 @@
 export const TERMS = {
     'EXISTING_URL':'Url already exists.',
     'EXPIRED_URL':'Sorry Url got expired.',
-    'NOT_FOUND_URL':'Sorry Url not found.'
+    'NOT_FOUND_URL':'Sorry Url not found.',
+    'ROLE_SUPER_ADMIN':'super-admin'
 }
