5294093d30e48e9c9d4e4dee80cbb20f9fe9ca5f
Naieem Mahmud Supto <naieemsupto@gmail.com>
2022-11-14 20:50:25 +0600

    [JWT] jwt module and configuration added

--- src/url_shortner/url_shortner.module.ts
+++ src/url_shortner/url_shortner.module.ts
@@ -1,7 +1,10 @@
 import { DynamicModule, Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthController } from './controller/auth.controller';
 import { UrlShortnerController } from './controller/url_shortner.controller';
 import { Url, UrlSchema } from './schema/url.schema';
+import { TokenService } from './services/token.service';
 import { UrlService } from './services/url_shortner.service';
 
 @Module({})
@@ -17,9 +20,15 @@ export class UrlShortnerModule {
                         schema: UrlSchema,
                     }
                 ]),
+                JwtModule.register({
+                    secret: process.env.JWT_SECRET,
+                    signOptions: {
+                        expiresIn: process.env.JWT_EXPIRES,
+                    }
+                }),
             ],
-            controllers: [UrlShortnerController],
-            providers: [UrlService],
+            controllers: [UrlShortnerController, AuthController],
+            providers: [UrlService, TokenService],
             exports: [],
         };
     }
