643058105fe9e086cc80d49e823a42597ff35a4e
naieem <naieemsupto@gmail.com>
2022-11-15 18:10:31 +0600

    [Exception] Global Exception handler implementation updated

--- src/utils/exception.handler.ts
+++ src/utils/exception.handler.ts
@@ -6,14 +6,10 @@ export class HttpExceptionFilter implements ExceptionFilter {
     catch(exception: HttpException, host: ArgumentsHost) {
         const ctx = host.switchToHttp();
         const response = ctx.getResponse<Response>();
-        const request = ctx.getRequest<Request>();
         const status = exception.getStatus();
+        const message = exception.getResponse()
         response
             .status(status)
-            .json({
-                errorType:exception.name,
-                payload: request.body,
-                message: exception.message,
-            });
+            .json(message);
     }
 }
