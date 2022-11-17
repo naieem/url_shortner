16595d676dd6c7a31e78fe0824de10a8cd580caf
Naieem Mahmud Supto <naieemsupto@gmail.com>
2022-11-17 17:18:09 +0600

    [TEST] Pure functions test case done

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
+import { AppModule } from './app.module';
 import { AppService } from './app.service';
+import { urlValidator } from './utils/constants';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -18,5 +20,15 @@ describe('AppController', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
+  });
+});
+describe('UrlValidator', () => {
+  it('Url validator generic function false output check(jeldfk)', () => {
+    const isValid = urlValidator('jeldfk');
+    expect(isValid).toBe(false);
+  });
+  it('Url validator generic function true output check(http://google.com)', () => {
+    const isValid = urlValidator('http://google.com');
+    expect(isValid).toBeTruthy();
   });
 });
