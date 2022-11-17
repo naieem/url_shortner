0adb6e6c79d039f810413a1a5f273aa9c408fb27
Naieem Mahmud Supto <naieemsupto@gmail.com>
2022-11-17 19:12:07 +0600

    [Feature] Shortcode optional done

--- src/url_shortner/dtos/shortcode.dto.ts
+++ src/url_shortner/dtos/shortcode.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class CreateShortCodeDTO {
     @ApiProperty({
@@ -14,7 +14,7 @@ export class CreateShortCodeDTO {
     @IsString()
     expiryDate: Date;
 
-    @IsNotEmpty()
+    @IsOptional()
     @IsString()
     shortCode?: String;
 }
